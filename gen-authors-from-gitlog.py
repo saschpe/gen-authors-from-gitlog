cce31b706bb8dae56f48b86b38c38596d646d082
Sascha Peilicke <saschpe@suse.de>
2010-11-05 10:06:10 +0100

    Added license header

--- gen-authors-from-gitlog.py
+++ gen-authors-from-gitlog.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python
+#
+# Copyright (c) 2010, Sascha Peilicke <saschpe@suse.de>, Novell Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program (see the file COPYING); if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
 
 import sys
 
